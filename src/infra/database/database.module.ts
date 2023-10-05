c804a73339c8ad6304b7cfa861b7109d597678b6
Felipe Sakaguti <felipesakaguti@hotmail.com>
2023-10-04 23:42:19 -0300

    refactor: authentication

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -7,6 +7,8 @@ import { PrismaQuestionsRepository } from './prisma/repositories/prisma-question
 import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
 import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-question-attachments-repository'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { StudentsRepository } from '@/domain/forum/application/repositories/students-repository'
+import { PrismaStudentsRepository } from './prisma/repositories/prisma-students-repository'
 
 @Module({
   providers: [
@@ -18,6 +20,10 @@ import { QuestionsRepository } from '@/domain/forum/application/repositories/que
       provide: QuestionsRepository,
       useClass: PrismaQuestionsRepository,
     },
+    {
+      provide: StudentsRepository,
+      useClass: PrismaStudentsRepository,
+    },
     PrismaQuestionCommentsRepository,
     PrismaQuestionAttachmentsRepository,
   ],
@@ -27,6 +33,7 @@ import { QuestionsRepository } from '@/domain/forum/application/repositories/que
     PrismaAnswerCommentsRepository,
     PrismaAnswerAttachmentsRepository,
     QuestionsRepository,
+    StudentsRepository,
     PrismaQuestionCommentsRepository,
     PrismaQuestionAttachmentsRepository,
   ],
