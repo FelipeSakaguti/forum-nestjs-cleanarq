32ba9653bf389b28a30d28667b6ae3eaf20b1fb2
Felipe Sakaguti <felipesakaguti@hotmail.com>
2023-10-05 12:06:22 -0300

    feat: comments and attachments migration and mappers

---
+++ src/infra/database/prisma/mappers/prisma-question-comment-mapper.ts
@@ -0,0 +1,36 @@
+import { Comment as PrismaComment, Prisma } from '@prisma/client'
+
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
+
+export class PrismaQuestionCommentMapper {
+  static toDomain(raw: PrismaComment) {
+    if (!raw.questionId) {
+      throw new Error('Invalid comment type.')
+    }
+
+    return QuestionComment.create(
+      {
+        content: raw.content,
+        authorId: new UniqueEntityId(raw.authorId),
+        questionId: new UniqueEntityId(raw.questionId),
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+      },
+      new UniqueEntityId(raw.id),
+    )
+  }
+
+  static toPrisma(
+    questionComment: QuestionComment,
+  ): Prisma.CommentUncheckedCreateInput {
+    return {
+      id: questionComment.id.toString(),
+      content: questionComment.content,
+      questionId: questionComment.questionId.toString(),
+      authorId: questionComment.authorId.toString(),
+      createdAt: questionComment.createdAt,
+      updatedAt: questionComment.updatedAt,
+    }
+  }
+}
