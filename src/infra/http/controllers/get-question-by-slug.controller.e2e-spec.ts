086e39195d4a74ed02a557e4aada552d7b3b08be
Felipe Sakaguti <felipesakaguti@hotmail.com>
2023-10-05 18:50:10 -0300

    tests: added factories

--- src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
+++ src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
@@ -1,3 +1,4 @@
+import { Slug } from '@/domain/forum/enterprise/entities/value-objetcts/slug'
 import { AppModule } from '@/infra/app.module'
 import { DatabaseModule } from '@/infra/database/database.module'
 import { INestApplication } from '@nestjs/common'
@@ -7,7 +8,7 @@ import request from 'supertest'
 import { QuestionFactory } from 'test/factories/make-question'
 import { StudentFactory } from 'test/factories/make-student'
 
-describe('Fetch recent Question (E2E)', () => {
+describe('Get Question By Slug (E2E)', () => {
   let app: INestApplication
   let studentFactory: StudentFactory
   let questionFactory: QuestionFactory
@@ -19,41 +20,32 @@ describe('Fetch recent Question (E2E)', () => {
       providers: [StudentFactory, QuestionFactory],
     }).compile()
 
-    jwt = moduleRef.get(JwtService)
     studentFactory = moduleRef.get(StudentFactory)
     questionFactory = moduleRef.get(QuestionFactory)
+    jwt = moduleRef.get(JwtService)
 
     app = moduleRef.createNestApplication()
     await app.init()
   })
 
-  test('[GET] /questions', async () => {
+  test('[GET] /questions/:slug', async () => {
     const user = await studentFactory.makePrismaStudent()
 
     const accessToken = jwt.sign({ sub: user.id.toString() })
 
-    await Promise.all([
-      questionFactory.makePrismaQuestion({
-        authorId: user.id,
-        title: 'Titulo teste',
-      }),
-      questionFactory.makePrismaQuestion({
-        authorId: user.id,
-        title: 'Titulo teste 2',
-      }),
-    ])
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+      slug: Slug.create('question-01'),
+    })
 
     const response = await request(app.getHttpServer())
-      .get('/questions')
+      .get(`/questions/question-01`)
       .set('Authorization', `Bearer ${accessToken}`)
       .send()
 
     expect(response.statusCode).toBe(200)
     expect(response.body).toEqual({
-      questions: [
-        expect.objectContaining({ title: 'Titulo teste 2' }),
-        expect.objectContaining({ title: 'Titulo teste' }),
-      ],
+      question: expect.objectContaining({ title: question.title }),
     })
   })
 })
--- test/factories/make-student.ts
+++ test/factories/make-student.ts
@@ -3,6 +3,9 @@ import {
   Student,
   StudentProps,
 } from '@/domain/forum/enterprise/entities/student'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { PrismaStudentMapper } from '@/infra/database/prisma/mappers/prisma-student-mapper'
+import { Injectable } from '@nestjs/common'
 
 export function makeStudent(override: Partial<StudentProps> = {}) {
   const student = Student.create({
@@ -13,4 +16,19 @@ export function makeStudent(override: Partial<StudentProps> = {}) {
   })
 
   return student
+}
+
+@Injectable()
+export class StudentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaStudent(data: Partial<StudentProps> = {}): Promise<Student> {
+    const student = makeStudent(data)
+
+    await this.prisma.user.create({
+      data: PrismaStudentMapper.toPrisma(student),
+    })
+
+    return student
+  }
 }
